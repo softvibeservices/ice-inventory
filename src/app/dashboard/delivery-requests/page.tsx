5562d188df4095041e2e0cfcc7302cd867ccd314
softvibeservices <softvibeservices@gmail.com>
2026-01-18 01:40:22 +0530

    small change

--- src/app/dashboard/delivery-requests/page.tsx
+++ src/app/dashboard/delivery-requests/page.tsx
@@ -196,7 +196,7 @@ export default function DeliveryRequestsPage() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <DashboardNavbar userId={userId ?? undefined} />
+      <DashboardNavbar />
 
       <main className="flex-grow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
